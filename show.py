85b4665c45e1b0effca0ce7e3150431fe978393d
Amishiki Kazuki(網敷　和樹) <2000129@tc.terumo.co.jp>
2024-03-11 22:34:05 +0900

    show.py update

--- show.py
+++ show.py
@@ -1,6 +1,6 @@
 import random
 
-with open("./hyakunin.txt", endoding="utf-8") as f:
+with open("./hyakunin.txt", encoding="UTF-8") as f:
     wakas = [s.strip() for s in f.readlines()]
 
 print(wakas[random.randrange(len(wakas))])
